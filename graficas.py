97ecf3716cd198e5cfd600bba20d126bdc79c66a
David Torres <juandis0246@gmail.com>
2024-02-15 22:42:59 -0500

    Para presentar

--- graficas.py
+++ graficas.py
@@ -1,17 +1,14 @@
 import matplotlib.pyplot as plot
-import numpy as np
 import sympy as sp
 
 # Variables or constants
 t = sp.symbols('t')
 a = sp.symbols('a')
 w = sp.symbols('w')
-phi = sp.symbols('phi')
 
 # Warning: I was looking for information, and google says phi can be variable and also can be a constant
 # for me is more comfortable to use it as a constant.
-
-phi = 1.618
+phi = 1.68
 
 
 # Functions (v(t) and a(t))
@@ -21,21 +18,69 @@ function_xt = a*sp.cos(w*t + phi)
 
 
 # Function to graph function
-def graph_function(function, title, x_title, y_title, outplut_name = "f.png"):
-    t_interval = [i for i in range(-50, 50)]
+def get_list_of_points(function, start = -50, end = 50, step = 0.1):
+
+    t_interval = []
     v_evalated_in_intervals = []
+
+    i = start
+    while True:
+        t_interval.append(i)
+        i += step
+        if i > end:
+            break
+
     for time in t_interval: # This is the x
-        v = function.subs({a: 1, w: 1, phi: 0, t: time}) # This is the y
+        v = function.subs({a: 1, w: 1, t: time}) # This is the y
         v_evalated_in_intervals.append(v)
-    plot.plot(t_interval, v_evalated_in_intervals)
-    plot.title(title)
-    plot.xlabel(x_title)
-    plot.ylabel(y_title)
-    plot.savefig(outplut_name)
 
-# Warning: I tried to invoke the same function at the same time, but it get bugged!!!
+    return t_interval, v_evalated_in_intervals
 
-#graph_function(function_vt, 'v(t)', 't', 'v', 'vt.png')
-#graph_function(function_at, 'a(t)', 't', 'v', 'at.png')
-#graph_function(function_xt, 'x(t)', 't', 'v', 'xt.png')
-    
+def option_menu():
+    print("1. v(t)")
+    print("2. a(t)")
+    print("3. x(t)")
+    print("4. All functions")
+    print("5. Another")
+    option = input("Choose an option: ")
+    return option
+
+def main():
+    option = option_menu()
+    start = int(input("Start: "))
+    end = int(input("End: "))
+    step = float(input("Step: "))
+    if option == "1":
+        t, v = get_list_of_points(function_vt, start, end, step)
+        plot.plot(t, v)
+        plot.title("v(t)")
+        plot.xlabel("t")
+        plot.ylabel("v(t)")
+        plot.savefig("v.png")
+    if option == "2":
+        t, a = get_list_of_points(function_at, start, end, step)
+        plot.plot(t, a)
+        plot.title("a(t)")
+        plot.xlabel("t")
+        plot.ylabel("a(t)")
+        plot.savefig("a.png")
+    if option == "3":
+        t, x = get_list_of_points(function_xt, start, end, step)
+        plot.plot(t, x)
+        plot.title("x(t)")
+        plot.xlabel("t")
+        plot.ylabel("x(t)")
+        plot.savefig("x.png")
+    if option == "4":
+        t, v = get_list_of_points(function_vt, start, end, step)
+        plot.plot(t, v)
+        t, a = get_list_of_points(function_at, start, end, step)
+        plot.plot(t, a)
+        t, x = get_list_of_points(function_xt, start, end, step)
+        plot.plot(t, x)
+        plot.title("All functions")
+        plot.xlabel("t")
+        plot.ylabel("f(t)")
+        plot.savefig("all.png")
+
+main()
